abdd3d318ac860c52bd4092a2771ecaf7615d27c
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-03-18 06:02:01 +0530

    Update HelloWorld.java

--- XXHR/java/server/HelloWorld.java
+++ XXHR/java/server/HelloWorld.java
@@ -1,11 +1,9 @@
 import java.util.*;
-public class HelloWorld{
-  public static void main(String args[])
-  {
-    //
-    System.out.println("HelloWorld");
-  }
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        List list = new ArrayList(); // raw type
+        list.add("Hello");
+        System.out.println(list.get(0));
+    }
 }
-
-
-///
