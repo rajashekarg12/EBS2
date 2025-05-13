e96139855112973035cad07f77d4198d513c6756
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-05-13 15:10:03 +0530

    Update vegInsert.sql

--- XXHR/sql/vegInsert.sql
+++ XXHR/sql/vegInsert.sql
@@ -1,10 +1,1 @@
-CREATE TABLE vegetable (
-    vegetable_id NUMBER PRIMARY KEY,
-    name VARCHAR2(100),
-    color VARCHAR2(50),
-    price_per_kg NUMBER
-);
-
-INSERT INTO vegetable (vegetable_id, name, color, price_per_kg) VALUES (1, 'Carrot', 'Orange', 40);
-INSERT INTO vegetable (vegetable_id, name, color, price_per_kg) VALUES (2, 'Spinach', 'Green', 30);
-INSERT INTO vegetable (vegetable_id, name, color, price_per_kg) VALUES (3, 'Beetroot', 'Purple', 50);
+select * from Dual;
